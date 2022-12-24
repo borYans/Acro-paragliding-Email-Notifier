f38eaba3b9bcf93425fdfc0aff4b5e7dc299cdce
Boris Janevski <Boris.Janevski@netcetera.com>
2022-12-24 22:55:56 +0100

    adding error handling from the response

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ LONGITUDE = "21.25"
 
 url = BASE_URL + "lat=" + LATITUDE + "&lon=" + LONGITUDE + "&appid=" + API_KEY
 
-response = requests.get(url).json()
-five_weather_data = response['list']
-city = response['city']
-potential_flyable_days = day_type.extract_day_forecast_object(five_weather_data, city)
-valid_flyable_days = day_type.calculate_conditions_and_prepare_mail(potential_flyable_days)
-valid_flyable_forecasts = day_type.get_flyable_forecasts(valid_flyable_days)
-mail = day_type.compose_mail(valid_flyable_forecasts, city)
-if mail != day_type.NOT_FLYABLE_DAY_TYPE:
-    email.send_mail(mail_subject="Acro na Osoj - DETALNA PROGNOZA", mail_content=mail)
+response_body = requests.get(url)
+if response_body.status_code == 200:
+    response = response_body.json()
+    five_weather_data = response['list']
+    city = response['city']
+    potential_flyable_days = day_type.extract_day_forecast_object(five_weather_data, city)
+    valid_flyable_days = day_type.calculate_conditions_and_prepare_mail(potential_flyable_days)
+    valid_flyable_forecasts = day_type.get_flyable_forecasts(valid_flyable_days)
+    mail = day_type.compose_mail(valid_flyable_forecasts, city)
+    if mail != day_type.NOT_FLYABLE_DAY_TYPE:
+        email.send_mail(mail_subject="Acro na Osoj - DETALNA PROGNOZA", mail_content=mail)
+else:
+    email.send_mail(mail_subject="Acro na Osoj - DETALNA ANALIZA",
+                    mail_content="Ne sme vo moznost da dobieme podatoci od vremenska prognoza.")
