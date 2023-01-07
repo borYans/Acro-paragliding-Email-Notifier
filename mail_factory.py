ef503549ba9d68c437c4c2a89f2bf9e6496a244c
Boris Janevski <Boris.Janevski@netcetera.com>
2023-01-07 14:42:13 +0100

    refactor

--- mail_factory.py
+++ mail_factory.py
@@ -17,6 +17,17 @@ def get_mails_from_config():
     return emails
 
 
+def get_mail_content(wind, date, wind_gust):
+    return f"Zdravo letaciste, imas letacki den na {date}!\n" \
+           f"Veter na start: {wind}m/s so udari {wind_gust}m/s.\n" \
+           f"Mailov se isprakja sekojpat koga parametrite " \
+           f"se zadovoleni za odredenata lokacija.\nNapolni instrumenti, " \
+           f"spremi kamera i stavi eden par rakavici vo opremata.\n" \
+           f"Se gledame na start!\n\n" \
+           f"Pozdrav,\n" \
+           f"Acromacedonia"
+
+
 class MailFactory:
     @staticmethod
     def send_mail(mail_type, wind, wind_gust, date):
@@ -24,27 +35,13 @@ class MailFactory:
             e_mail = Mail(
                 to=get_mails_from_config(),
                 subject="Letacki den na VODNO",
-                body=f"Zdravo letaciste, imas letacki den na {date}!\n"
-                     f"Veter na start: {wind}m/s so udari {wind_gust}m/s.\n"
-                     f"Mailov se isprakja sekojpat koga parametrite "
-                     f"se zadovoleni za odredenata lokacija.\nNapolni instrumenti, "
-                     f"spremi kamera i stavi eden par rakavici vo opremata.\n"
-                     f"Se gledame na start!\n\n"
-                     f"Pozdrav,\n"
-                     f"Acromacedonia"
+                body=get_mail_content(wind, date, wind_gust)
             )
             mail.send_mail(e_mail)
         elif mail_type == 'Osoj':
             e_mail = Mail(
                 to=get_mails_from_config(),
                 subject="Letacki den na OSOJ",
-                body=f"Zdravo letaciste, imas letacki den na {date}!\n"
-                     f"Veter na start: {wind}m/s so udari {wind_gust}m/s.\n"
-                     f"Mailov se isprakja sekojpat koga parametrite "
-                     f"se zadovoleni za odredenata lokacija.\nNapolni instrumenti, "
-                     f"spremi kamera i stavi eden par rakavici vo opremata.\n"
-                     f"Se gledame na start!\n\n"
-                     f"Pozdrav,\n\n"
-                     f"Acromacedonia"
+                body=get_mail_content(wind, date, wind_gust)
             )
             mail.send_mail(e_mail)
